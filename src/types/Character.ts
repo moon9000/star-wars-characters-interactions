cd8df4224babf9c271f78ccae9ef457f5186e68a
Sabri <sabri.abdennader@wandi.fr>
2022-11-11 17:54:27 +0100

    feat(src): add types folder for types

---
+++ src/types/Character.ts
@@ -0,0 +1,18 @@
+export type Character = {
+  name: string;
+  height: number;
+  mass: number;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  url: string;
+};
+
+export type AllCharactersQuery = {
+  results: Character[];
+};
